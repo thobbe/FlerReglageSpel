e284dbbd2635a5013d3601d47090f3df553133ec
Erikmannen <erik.azp@hotmail.se>
2018-03-19 08:26:33 +0100

    ss

--- Nått kul/Assets/Scripts/start_scene/EnterSpaceship.cs
+++ Nått kul/Assets/Scripts/start_scene/EnterSpaceship.cs
@@ -6,12 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class EnterSpaceship: MonoBehaviour
 {
+    public string sceneName;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
-		{
-			SceneManager.LoadScene("scene_Erik");
-		}
-	}
+        {
+            StartCoroutine(Pause());
+        }
+    }
+
+    IEnumerator Pause()
+    {
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = f;
+            GetComponent<Renderer>().material.color = c;
+            yield return new WaitForSeconds(0.1f);
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+
 }
