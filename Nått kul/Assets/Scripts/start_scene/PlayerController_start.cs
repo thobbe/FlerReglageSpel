0dc4287c81b0d69bdf48fb925a0e8fc101662f82
Erikmannen <erik.azp@hotmail.se>
2018-04-14 14:24:02 +0200

    start_modeller

---
+++ Nått kul/Assets/Scripts/start_scene/PlayerController_start.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_start : MonoBehaviour {
+
+    public float speed;
+    public float rotateSpeed;
+    private CharacterController cc;
+
+    // Use this for initialization
+    void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cc = GetComponent<CharacterController>();
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        cc.SimpleMove(forward * curSpeed);
+
+    }
+}
