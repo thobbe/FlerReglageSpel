5f20308eab86b8da1bb8fcf44e520cebf9dfb468
Malin Niska <malni241@student.liu.se>
2018-04-17 11:55:24 +0200

    Malins scene

---
+++ Nått kul/Assets/Scripts/scene_Malin/Rotator_movement_malin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* ***************************************************
+ * 
+ * 			  Får "mission" att röra sig 
+ * 
+ * 
+ * **************************************************/
+
+public class Rotator_movement_malin : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
