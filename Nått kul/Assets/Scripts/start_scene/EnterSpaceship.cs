939017e59af445e06957c7c78175f43f70faa5bc
Erikmannen <erik.azp@hotmail.se>
2018-04-18 14:14:19 +0200

    c

--- Nått kul/Assets/Scripts/start_scene/EnterSpaceship.cs
+++ Nått kul/Assets/Scripts/start_scene/EnterSpaceship.cs
@@ -6,12 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class EnterSpaceship: MonoBehaviour
 {
+    //Takes name of scene written in Unity
     public string sceneName;
 
+    //Function with collider that uses isTrigger
 	private void OnTriggerEnter(Collider other)
 	{
+        //Uses tag on Player model K1
 		if (other.CompareTag("Player"))
         {
+            //Calls Fade function that uses the scene name, color to fade in and how long the fade is going to be
             Initiate.Fade(sceneName, Color.black, 2.0f);
         }
     }
